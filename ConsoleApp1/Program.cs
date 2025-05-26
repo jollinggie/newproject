f83cfaa8fc942f23bc1a2ab378cf33caffd06443
Angelina <jollinggie@gmail.com>
2025-05-26 10:48:18 +0800

    0526 practice

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 按F5试试, f5，是不是没有按fn, 这个也是快速run，好啦，niwanba
+//这个是toplevel，你没发现没有Main function么，这些都被省略了现在，你复制这个block里面的
+string Dogname = "蛋挞";
+float age = 10000000;
+Console.WriteLine("my dog " + Dogname+" is gonna live to " + age+" years old!");
