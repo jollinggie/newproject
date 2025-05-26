e76932e8bc04b3e4f32354570140686c3a05e91a
shaoqiu Pan <stephenpsq@gmail.com>
2025-05-26 11:41:59 +0800

    week1 assignment

---
+++ week1/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("这是Task1");
+
+
+// 题目描述
+// 编写一个程序，提示用户输入一个正整数 n，然后使用嵌套循环打印一个直角三角形，形状如下（以 n=5 为例）：
+
+// markdown
+// Copy
+// Edit
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// 外层循环控制行数 i 从 1 到 n；
+
+// 内层循环控制每行打印的星号数量 j 从 1 到 i；
+
+// 星号之间用一个空格分隔；
+
+// 每打印完一行内层循环后输出换行。
